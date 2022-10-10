5b5689f92630f2540e6e78ba4465b1dd8af505ff
webdeveloper0-trying <yoonnadimyomyint2221@gmail.com>
2022-10-10 10:51:35 +0630

    Coding Folder Uploaded

---
+++ project_name/patient/View/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <!-- root Css -->
+    <link rel="stylesheet" href="./resources/css/root.css">
+    <!-- jquery -->
+    <script src="./resources/js/jquery3.6.0.js"></script>
+</head>
+
+<body>
+
+
+    <!-- Main Content -->
+    <div class="container">
+
+    </div>
+
+
+</body>
+
+</html>
